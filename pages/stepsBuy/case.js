d0ae949acfeab2502e012acc083fccd563d7ecef
myge <miguel.contreras1@mail.udp.cl>
2022-06-28 17:24:29 -0400

    a

--- pages/stepsBuy/case.js
+++ pages/stepsBuy/case.js
@@ -49,6 +49,18 @@ const productsList = {
     imageSrc: "/FotosMinijo/case/1.jpg",
     imageAlt: "RED",
   },
+  9: {
+    name: "FancyAlice Metal Black",
+    price: "$12000",
+    imageSrc: "/FotosMinijo/case/1.jpg",
+    imageAlt: "RED",
+  },
+    10: {
+    name: "FancyAlice Metal Black",
+    price: "$12000",
+    imageSrc: "/FotosMinijo/case/1.jpg",
+    imageAlt: "RED",
+  },
 };
 
 const steps = [
